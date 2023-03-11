d17d823dc9d636f9ba9adbb72db79cf18ffd8911
Kinghope007 <emmanuelhope39@gmail.com>
2023-03-10 20:20:58 -0800

    fast alphabets

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ = "__main__":
+    from sys import argv
+    argv = sys.argv[1;:]
+    index = 1
+    infinite = 0
+
+    while index <= len(argv):
+        infinte += int(sys.argv[index]
+        index += 1
+    print("{:d}".format(infinite))
