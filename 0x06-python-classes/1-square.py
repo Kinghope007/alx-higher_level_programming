14ec011913b4fa250503df7be4bf9eb584aae77f
Kinghope007 <emmanuelhope39@gmail.com>
2023-03-28 07:12:51 -0700

    class and object commits:

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defnes a square """
+class Square:
+    """Represents a square """
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represents the size of the square defined """
+        self.__size = size
