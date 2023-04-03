ff6af6b01e36b13cea34a9488a3be91fc2953be0
Kinghope007 <emmanuelhope39@gmail.com>
2023-04-03 06:33:03 -0700

    rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
-#!/usr/python3
-"""A class that defines arectangle"""
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
 
 
 class Rectangle:
-    """"An empty class that defines a rectangle"""
+    """An empty class that defines a ractangle"""
     pass
