7368151e93028997d6ed754a10f87b8b557b7dd1
Kinghope007 <emmanuelhope39@gmail.com>
2023-04-13 07:02:59 -0700

    JSON

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a pascals Triangle function
+"""
+
+
+def pascal_triangle(n):
+    """
+    Represent Pascal Triangle of size n
+    """
+    if n <= 0:
+        return []
+
+
