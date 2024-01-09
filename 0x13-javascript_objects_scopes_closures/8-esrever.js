e6b6eb32859a5a971d92d2e6a6b617db90516b03
Kinghope007 <emmanuelhope39@gmail.com>
2024-01-09 13:35:56 -0800

    missing #!

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,8 @@
+#!/usr/bin/node
 exports.esrever = function (list) {
   // Create a new array to store the reversed elements
   const reversedList = [];
-  // Iterate through the original list in reverse order and push each element to the new array
+  // hrough the original list in reverse order and push each element to the new array
   for (let i = list.length - 1; i >= 0; i--) {
     reversedList.push(list[i]);
   }
