9a9d5838eea86597851ea07a98a3540f9a84b133
Kinghope007 <emmanuelhope39@gmail.com>
2024-01-25 06:26:11 -0800

    cut

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# Check if url is provided s an argumrnt
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit
-fi
-
 # Use curl to send a request and display the size of the body in bytes
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+curl -sI "$1" | grep -i "Content-Length" | cut -d ' ' -f2
